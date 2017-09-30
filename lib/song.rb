ca779e4802acb0e8d5125f141b7e9e4a98bf7c1e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 22:01:14 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,8 +7,9 @@ class Song
     @name = name
   end
 
-  def artist_name
+  def artist_name(name)
     self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song = self
   end
 
 def self.new_by_filename(filename)
