1e30cc0a2d5c5d607e8394dcc6f0871b72e4e9c8
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 21:44:04 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -7,6 +7,14 @@ class Song
     @name = name
   end
 
+  def artist_name
+
+def new_by_filename(filename)
+  song = filename.split(" - ")[1]
+  artist = filename.split(" - ")[0]
+  songs = self.new(song)
+  songs.artist_name = artist
+end
 
 
 
