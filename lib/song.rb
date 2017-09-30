973937c0b55303b2c547a26423794cecf1cd75a8
Mariana Sickler <marianaleal_93@hotmail.com>
2017-09-30 21:57:38 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -11,11 +11,12 @@ class Song
     self.artist = Artist.find_or_create_by_name(name)
   end
 
-def new_by_filename(filename)
+def self.new_by_filename(filename)
   song = filename.split(" - ")[1]
   artist = filename.split(" - ")[0]
   songs = self.new(song)
   songs.artist_name = artist
+  songs
 end
 
 
